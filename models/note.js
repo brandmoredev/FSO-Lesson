30f0372f9b85fc04e8ec6cd85cfa49f86f533e6c
Brando <brandmoredev@gmail.com>
2025-04-05 03:36:41 -0700

    Modularize MVC

--- models/note.js
+++ models/note.js
@@ -1,22 +1,3 @@
-const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
